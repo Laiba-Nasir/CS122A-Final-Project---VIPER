1a5e78dc923154a199f4b56eaa091053838f2187
Samantha Placito Melendrez <smelendrez002@gmail.com>
2026-06-06 22:30:34 -0700

    fixed code and got rid of dummy_tx.

--- pico/src/spi/spi_master.h
+++ pico/src/spi/spi_master.h
@@ -1,1 +1,18 @@
 // read_centroid() header
+#ifndef SPI_MASTER_H
+#define SPI_MASTER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//seperate header file from our .c code to make it easier to call our spi logic in main.c
+typedef struct{
+    uint16_t centroid_x;
+    uint16_t centroid_y;
+    bool color_detected;
+} fpga_centroid;
+
+void spi_master_init(void);
+bool centroid_read(fpga_centroid* centroid);
+
+#endif // SPI_MASTER_H
